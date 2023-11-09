fb7bb9369528dd94f419a64ec39dca8c070e1452
antoniiaaaa <antoniiaaaa.m@gmail.com>
2023-11-09 16:29:00 +0700

    modified:   application/__pycache__/form.cpython-311.pyc 	modified:   application/__pycache__/routes.cpython-311.pyc 	modified:   application/form.py 	modified:   application/routes.py 	new file:   application/static/images/posts/a2d2e723de.png 	new file:   application/static/images/posts/bdaef9089b.png 	modified:   application/views/components/footer.html 	modified:   application/views/components/login-header.html 	modified:   application/views/components/logout-header.html 	modified:   application/views/createPosts.html 	modified:   application/views/editProfile.html 	modified:   application/views/index.html 	modified:   application/views/layout.html 	modified:   application/views/login.html 	modified:   application/views/profile.html 	modified:   application/views/signup.html 	new file:   ig.txt 	modified:   instance/data.db

--- application/routes.py
+++ application/routes.py
@@ -20,7 +20,7 @@ def login():
         user = User.query.filter_by(username=username).first()
         if user and password == user.password:
             login_user(user)
-            return redirect(url_for('profile', username=username))
+            return redirect(url_for('index', username=username))
         else:
             flash('Invalid username or password', 'error')
 
@@ -32,10 +32,30 @@ def logout():
     logout_user()
     return redirect(url_for('login'))
 
-# @app.route('/profile')
-# @login_required
-# def profile():
-#     return render_template('profile.html', title=f'{current_user.fullname} Profile')
+@app.route('/edit', methods=['GET', 'POST'])
+@login_required
+def edit():
+    form = EditProfileForm()
+
+    if form.validate_on_submit():
+        user = User.query.get(current_user.id)
+        if form.username.data != user.username:
+            user.username = form.username.data
+        user.fullname = form.fullname.data
+        user.bio = form.bio.data
+
+        if form.profile_pic.data:
+            pass
+            
+        db.session.commit()
+        flash('Profile Updated.', 'success')
+        return redirect(url_for('profile', username=current_user.username))
+
+    form.username.data = current_user.username
+    form.fullname.data = current_user.fullname
+    form.bio.data = current_user.bio
+
+    return render_template('editProfile.html', title=f'Edit {current_user.username}s Profile', form=form)
 
 @app.route('/<string:username>')
 @login_required
@@ -60,11 +80,11 @@ def index():
         db.session.commit()
         flash('Your image has been posted', 'success')
 
-    page = request.args.get('page', 1, type=int)
-    posts = Post.query.filter_by(author_id = current_user.id).order_by(Post.post_date.desc()).paginate(page=page, per_page=3)
+    # page = request.args.get('page', 1, type=int)
+    # posts = Post.query.filter_by(author_id = current_user.id).order_by(Post.post_date.desc()).paginate(page=page, per_page=3)
 
-    #without paginate (pages)
-    #posts = current_user.posts
+    # without paginate (pages)
+    posts = current_user.posts
 
     return render_template('index.html', title='Home', form=form, posts=posts)
 
@@ -93,6 +113,25 @@ def signup():
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+@app.route('/createPost')
+@login_required
+def create():
+    form = CreatePostForm()
+
+    if form.validate_on_submit():
+        post = Post(
+            author_id = current_user.id,
+            caption = form.caption.data
+        )
+        post.photo = save_image(form.post_pic.data)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your image has been posted', 'success')
+    posts = current_user.posts
+
+    return render_template('index.html', title='Home', form=form, posts=posts)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
